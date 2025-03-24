41dd931793731d30a2879db0df8a11a361908de5
Sergio Mauricio Campos Carranza <camposmauriciocarranza@outlook.com>
2025-03-23 22:22:23 -0600

    diseño y funcionamiento FormProyectos

--- StarGets/FormProyectos.Designer.cs
+++ StarGets/FormProyectos.Designer.cs
@@ -28,12 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvProyectos = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cbDepartamento = new System.Windows.Forms.ComboBox();
+            this.dtpInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtpEntrega = new System.Windows.Forms.DateTimePicker();
+            this.lblProyecto = new System.Windows.Forms.Label();
+            this.lblDepartamento = new System.Windows.Forms.Label();
+            this.lblFechaInicio = new System.Windows.Forms.Label();
+            this.lblFechaEtrega = new System.Windows.Forms.Label();
+            this.lblEstadoProyecto = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblObservaciones = new System.Windows.Forms.Label();
+            this.txtObservacion = new System.Windows.Forms.TextBox();
+            this.lblListaProyectos = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblNombreProyectos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProyectos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProyectos
+            // 
+            this.dgvProyectos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProyectos.Location = new System.Drawing.Point(12, 609);
+            this.dgvProyectos.Name = "dgvProyectos";
+            this.dgvProyectos.RowHeadersWidth = 51;
+            this.dgvProyectos.RowTemplate.Height = 24;
+            this.dgvProyectos.Size = new System.Drawing.Size(862, 175);
+            this.dgvProyectos.TabIndex = 0;
+            this.dgvProyectos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProyectos_CellContentClick);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(127, 136);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // cbDepartamento
+            // 
+            this.cbDepartamento.FormattingEnabled = true;
+            this.cbDepartamento.Location = new System.Drawing.Point(127, 174);
+            this.cbDepartamento.Name = "cbDepartamento";
+            this.cbDepartamento.Size = new System.Drawing.Size(200, 24);
+            this.cbDepartamento.TabIndex = 2;
+            // 
+            // dtpInicio
+            // 
+            this.dtpInicio.Location = new System.Drawing.Point(127, 223);
+            this.dtpInicio.Name = "dtpInicio";
+            this.dtpInicio.Size = new System.Drawing.Size(200, 22);
+            this.dtpInicio.TabIndex = 3;
+            // 
+            // dtpEntrega
+            // 
+            this.dtpEntrega.Location = new System.Drawing.Point(491, 223);
+            this.dtpEntrega.Name = "dtpEntrega";
+            this.dtpEntrega.Size = new System.Drawing.Size(200, 22);
+            this.dtpEntrega.TabIndex = 4;
+            // 
+            // lblProyecto
+            // 
+            this.lblProyecto.AutoSize = true;
+            this.lblProyecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProyecto.Location = new System.Drawing.Point(15, 136);
+            this.lblProyecto.Name = "lblProyecto";
+            this.lblProyecto.Size = new System.Drawing.Size(66, 18);
+            this.lblProyecto.TabIndex = 5;
+            this.lblProyecto.Text = "Nombre:";
+            // 
+            // lblDepartamento
+            // 
+            this.lblDepartamento.AutoSize = true;
+            this.lblDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartamento.Location = new System.Drawing.Point(15, 180);
+            this.lblDepartamento.Name = "lblDepartamento";
+            this.lblDepartamento.Size = new System.Drawing.Size(106, 18);
+            this.lblDepartamento.TabIndex = 6;
+            this.lblDepartamento.Text = "Departamento:";
+            // 
+            // lblFechaInicio
+            // 
+            this.lblFechaInicio.AutoSize = true;
+            this.lblFechaInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaInicio.Location = new System.Drawing.Point(15, 223);
+            this.lblFechaInicio.Name = "lblFechaInicio";
+            this.lblFechaInicio.Size = new System.Drawing.Size(111, 18);
+            this.lblFechaInicio.TabIndex = 7;
+            this.lblFechaInicio.Text = "Fecha de Inicio:";
+            // 
+            // lblFechaEtrega
+            // 
+            this.lblFechaEtrega.AutoSize = true;
+            this.lblFechaEtrega.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaEtrega.Location = new System.Drawing.Point(339, 226);
+            this.lblFechaEtrega.Name = "lblFechaEtrega";
+            this.lblFechaEtrega.Size = new System.Drawing.Size(128, 18);
+            this.lblFechaEtrega.TabIndex = 8;
+            this.lblFechaEtrega.Text = "Fecha de Entrega:";
+            // 
+            // lblEstadoProyecto
+            // 
+            this.lblEstadoProyecto.AutoSize = true;
+            this.lblEstadoProyecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstadoProyecto.Location = new System.Drawing.Point(15, 262);
+            this.lblEstadoProyecto.Name = "lblEstadoProyecto";
+            this.lblEstadoProyecto.Size = new System.Drawing.Size(59, 18);
+            this.lblEstadoProyecto.TabIndex = 9;
+            this.lblEstadoProyecto.Text = "Estado:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(127, 261);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(200, 22);
+            this.txtEstado.TabIndex = 10;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(15, 304);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(91, 18);
+            this.lblDescripcion.TabIndex = 11;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescripcion.Location = new System.Drawing.Point(127, 300);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(564, 115);
+            this.txtDescripcion.TabIndex = 12;
+            // 
+            // lblObservaciones
+            // 
+            this.lblObservaciones.AutoSize = true;
+            this.lblObservaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblObservaciones.Location = new System.Drawing.Point(15, 455);
+            this.lblObservaciones.Name = "lblObservaciones";
+            this.lblObservaciones.Size = new System.Drawing.Size(112, 18);
+            this.lblObservaciones.TabIndex = 13;
+            this.lblObservaciones.Text = "Observaciones:";
+            // 
+            // txtObservacion
+            // 
+            this.txtObservacion.Location = new System.Drawing.Point(127, 447);
+            this.txtObservacion.Multiline = true;
+            this.txtObservacion.Name = "txtObservacion";
+            this.txtObservacion.Size = new System.Drawing.Size(564, 104);
+            this.txtObservacion.TabIndex = 14;
+            // 
+            // lblListaProyectos
+            // 
+            this.lblListaProyectos.AutoSize = true;
+            this.lblListaProyectos.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListaProyectos.Location = new System.Drawing.Point(9, 574);
+            this.lblListaProyectos.Name = "lblListaProyectos";
+            this.lblListaProyectos.Size = new System.Drawing.Size(149, 19);
+            this.lblListaProyectos.TabIndex = 15;
+            this.lblListaProyectos.Text = "Lista de Proyectos:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(12, 66);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(211, 31);
+            this.btnAgregar.TabIndex = 16;
+            this.btnAgregar.Text = "Agregar nuevo proyecto";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(229, 66);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(211, 31);
+            this.btnActualizar.TabIndex = 17;
+            this.btnActualizar.Text = "Actualizar proyecto";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(446, 66);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(211, 31);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "Eliminar proyecto";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblNombreProyectos
+            // 
+            this.lblNombreProyectos.AutoSize = true;
+            this.lblNombreProyectos.Font = new System.Drawing.Font("Microsoft JhengHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProyectos.Location = new System.Drawing.Point(12, 9);
+            this.lblNombreProyectos.Name = "lblNombreProyectos";
+            this.lblNombreProyectos.Size = new System.Drawing.Size(149, 36);
+            this.lblNombreProyectos.TabIndex = 20;
+            this.lblNombreProyectos.Text = "Proyectos";
+            // 
+            // FormProyectos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(942, 796);
+            this.Controls.Add(this.lblNombreProyectos);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblListaProyectos);
+            this.Controls.Add(this.txtObservacion);
+            this.Controls.Add(this.lblObservaciones);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.lblEstadoProyecto);
+            this.Controls.Add(this.lblFechaEtrega);
+            this.Controls.Add(this.lblFechaInicio);
+            this.Controls.Add(this.lblDepartamento);
+            this.Controls.Add(this.lblProyecto);
+            this.Controls.Add(this.dtpEntrega);
+            this.Controls.Add(this.dtpInicio);
+            this.Controls.Add(this.cbDepartamento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgvProyectos);
+            this.Name = "FormProyectos";
             this.Text = "FormProyectos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProyectos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProyectos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cbDepartamento;
+        private System.Windows.Forms.DateTimePicker dtpInicio;
+        private System.Windows.Forms.DateTimePicker dtpEntrega;
+        private System.Windows.Forms.Label lblProyecto;
+        private System.Windows.Forms.Label lblDepartamento;
+        private System.Windows.Forms.Label lblFechaInicio;
+        private System.Windows.Forms.Label lblFechaEtrega;
+        private System.Windows.Forms.Label lblEstadoProyecto;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblObservaciones;
+        private System.Windows.Forms.TextBox txtObservacion;
+        private System.Windows.Forms.Label lblListaProyectos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblNombreProyectos;
     }
 }
--- StarGets/FormProyectos.cs
+++ StarGets/FormProyectos.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,178 @@ namespace StarGets
 {
     public partial class FormProyectos: Form
     {
+        string connectionString = "Server=localhost;Database=nombre_de_tu_bd;Trusted_Connection=True;";
+
         public FormProyectos()
         {
             InitializeComponent();
+        }
+
+        private void FormProyectos_Load(object sender, EventArgs e)
+        {
+            CargarDepartamentos();
+            CargarProyectos();
+        }
+
+        private void CargarProyectos()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("select * from proyectos", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvProyectos.DataSource = dt;
+            }
+        }
+
+        private void CargarDepartamentos()
+        {
+            cbDepartamento.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "select id_departamento, nombre_departamento from departamentos";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cbDepartamento.Items.Add(new
+                    {
+                        Text = reader["nombre_departamento"].ToString(),
+                        Value = reader["id_departamento"]
+                    });
+                }
+            }
+
+            cbDepartamento.DisplayMember = "Text";
+            cbDepartamento.ValueMember = "Value";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (cbDepartamento.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona un departamento.");
+                return;
+            }
+
+            int idDepartamento = (int)((dynamic)cbDepartamento.SelectedItem).Value;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"
+                    insert into proyectos (id_departamento, nombre_proyecto, observacion, fecha_inicial, fecha_entrega, estado_proyecto, descripcion)
+                    values (@id_departamento, @nombre, @observacion, @inicio, @entrega, @estado, @descripcion)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id_departamento", idDepartamento);
+                cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@observacion", txtObservacion.Text);
+                cmd.Parameters.AddWithValue("@inicio", dtpInicio.Value);
+                cmd.Parameters.AddWithValue("@entrega", dtpEntrega.Value);
+                cmd.Parameters.AddWithValue("@estado", txtEstado.Text);
+                cmd.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Proyecto agregado.");
+            CargarProyectos();
+            LimpiarCampos();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (dgvProyectos.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona un proyecto.");
+                return;
+            }
+
+            int idProyecto = Convert.ToInt32(dgvProyectos.CurrentRow.Cells["id_proyecto"].Value);
+            int idDepartamento = (int)((dynamic)cbDepartamento.SelectedItem).Value;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"
+                    update proyectos
+                    set id_departamento = @id_departamento,
+                        nombre_proyecto = @nombre,
+                        observacion = @observacion,
+                        fecha_inicial = @inicio,
+                        fecha_entrega = @entrega,
+                        estado_proyecto = @estado,
+                        descripcion = @descripcion
+                    where id_proyecto = @id_proyecto";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id_departamento", idDepartamento);
+                cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                cmd.Parameters.AddWithValue("@observacion", txtObservacion.Text);
+                cmd.Parameters.AddWithValue("@inicio", dtpInicio.Value);
+                cmd.Parameters.AddWithValue("@entrega", dtpEntrega.Value);
+                cmd.Parameters.AddWithValue("@estado", txtEstado.Text);
+                cmd.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+                cmd.Parameters.AddWithValue("@id_proyecto", idProyecto);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Proyecto actualizado.");
+            CargarProyectos();
+            LimpiarCampos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvProyectos.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona un proyecto para eliminar.");
+                return;
+            }
+
+            int idProyecto = Convert.ToInt32(dgvProyectos.CurrentRow.Cells["id_proyecto"].Value);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("delete from proyectos where id_proyecto = @id", conn);
+                cmd.Parameters.AddWithValue("@id", idProyecto);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Proyecto eliminado.");
+            CargarProyectos();
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Clear();
+            txtEstado.Clear();
+            txtDescripcion.Clear();
+            txtObservacion.Clear();
+            cbDepartamento.SelectedIndex = -1;
+            dtpInicio.Value = DateTime.Now;
+            dtpEntrega.Value = DateTime.Now;
+        }
+
+
+        private void dgvProyectos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvProyectos.Rows[e.RowIndex];
+
+                txtNombre.Text = row.Cells["nombre_proyecto"].Value.ToString();
+                txtObservacion.Text = row.Cells["observacion"].Value.ToString();
+                dtpInicio.Value = Convert.ToDateTime(row.Cells["fecha_inicial"].Value);
+                dtpEntrega.Value = Convert.ToDateTime(row.Cells["fecha_entrega"].Value);
+                txtEstado.Text = row.Cells["estado_proyecto"].Value.ToString();
+                txtDescripcion.Text = row.Cells["descripcion"].Value.ToString();
+
+                // Puedes buscar el departamento en cbDepartamento si deseas
+            }
         }
     }
 }
